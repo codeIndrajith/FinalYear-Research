4486b0665904ac4778ec79d8d3fac3fde5310a1e
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-02-18 19:53:50 +0530

    add changes

--- back-end/controllers/auth.js
+++ back-end/controllers/auth.js
@@ -1,41 +1,45 @@
-const User = require('../models/User')
-const generateToken = require('../utils/generateToken');
+const User = require("../models/User");
+const generateToken = require("../utils/generateToken");
 const register = async (req, res, next) => {
-    try {
-        const existUser = await User.findOne({ email: req.body.email });
-        if(existUser) {
-            res.status(400);
-            throw new Error("User Already exist")
-        }
-
-        const user = await User.create(req.body)
-
-        if(user) {
-            generateToken(res, user._id);
-
-            res.status(201).json({success: true, message: "Registration complete"})
-        } else {
-            res.status(400);
-            throw new Error('Invalid user data');
-        }
-    } catch (error) {
-        next(error)
+  try {
+    const existUser = await User.findOne({ email: req.body.email });
+    if (existUser) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
     }
-}
+
+    const user = await User.create(req.body);
+
+    if (user) {
+      generateToken(res, user._id);
+
+      return res
+        .status(201)
+        .json({ success: true, message: "Registration complete" });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid user data" });
+    }
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 const login = async (req, res, next) => {
-    try {
-        const user = await User.findOne({email: req.body.email});
-        if(user && (await user.matchPassword(req.body.password))) {
-            generateToken(res, user._id);
-            res.status(200).json({success: true, message: "Auth complete"})
-        } else {
-            res.status(400);
-            throw new Error('Invalid email or password');
-        }
-    } catch (error) {
-        next(error)
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user && (await user.matchPassword(req.body.password))) {
+      generateToken(res, user._id);
+      res.status(200).json({ success: true, message: "Auth complete" });
+    } else {
+      res.status(400);
+      throw new Error("Invalid email or password");
     }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = {register, login};
+module.exports = { register, login };
--- back-end/server.js
+++ back-end/server.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const connectDB = require('./config/db')
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const {notFound, errorHandler} = require('./middleware/errorMiddleware')
-const tf = require('@tensorflow/tfjs');
-const fs = require('fs').promises;
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const tf = require("@tensorflow/tfjs");
+const fs = require("fs").promises;
 const PORT = 5000;
-const userRoutes = require('./routes/userRoutes');
-require('dotenv').config();
+const userRoutes = require("./routes/userRoutes");
+require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: true, // Reflects the request's origin
+    credentials: true, // Allows cookies and authentication headers
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,16 +29,16 @@ app.use(cookieParser());
 // DB connection
 connectDB();
 
-app.use('/api/v1/auth', userRoutes)
+app.use("/api/v1/auth", userRoutes);
 
-app.get('/predict', async (req, res) => {
+app.get("/predict", async (req, res) => {
   const modelPath = process.env.MODEL_PATH;
 
   try {
     res.status(200).json({ success: true, modelPath });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ success: false, msg: 'Model path is wrong' });
+    res.status(500).json({ success: false, msg: "Model path is wrong" });
   }
 });
 
